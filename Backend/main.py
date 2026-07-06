dc711f93eb27fb6e118ae0c6046ad4ba064c5b6a
Sakshi <sakshim4333@gmail.com>
2026-07-06 20:55:46 +0530

    frontend prod url changed

--- Backend/main.py
+++ Backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "https://cryptopulse-bay.vercel.app"
+    "https://cryptopulse-lemon-delta.vercel.app"
 ]
 
 app.add_middleware(
